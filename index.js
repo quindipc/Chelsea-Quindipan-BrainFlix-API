10e192da0e52a8e5d84e8e0eb4d8cdbfcf55b722
Chelsea Quindipan <quindipc@gmail.com>
2023-07-05 19:46:08 -0400

    update functionality and test get and post requests

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
+// DEPENDANCIES
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
-// Routes for the video
-const videoRoutes = require("./routes/vidoes");
+const fs = require("node:fs");
+const { v4 } = require("uuid");
 
 const app = express();
-
 const PORT = process.env.PORT || 5050;
+const DATA = "./data/videos.json"
+
+// Routes for the video
+const videoRoutes = require("./routes/videos");
+
 
 // Middleware -- json & allow CORS
 app.use(express.json());
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
+ 
+// GET /videos endpoint that responds with an array of videos
+app.get("/videos", (req, res) => {
+    const videos = readVideos();
+    res.json(videos);
+})
+
+// GET /videos/:id endpoint that response with an object containing the details of the video with an id
+app.get("/videos/:id", (req, res) => {
+    const videos = readVideos();
+    const video = videos.find((v) => v.id === req.params.id);
+    if (video) {
+        res.json(video);
+    } else {
+        res.status(404).json({ message: "Video not found" });
+    }
+});
+
+// POST /videos that will add a new video to the list & a unique ID
+app.post("/videos", (req, res) => {
+    const videos = readVideos();
+  
+    const { title, description, thumbnail } = req.body;
+  
+    const newVideo = {
+      id: uuidv4(),
+      title,
+      description,
+      thumbnail,
+    };
+  
+    videos.push(newVideo);
+    writeVideos(videos);
+    res.status(201).json(newVideo);
+  });
 
 // Register & Define Routes
 app.use("/videos", videoRoutes);
 
+// Write & Read JSON file
+function readVideos() {
+  const data = fs.readFileSync(DATA);
+  return JSON.parse(data);
+}
 
-// Test to see if running properly -- please delete later
-app.get("/status", (_req, res) => {
-    const status = {
-        "Status": "Running"
-    };
+function writeVideos(videos) {
+  fs.writeFileSync(DATA, JSON.stringify(videos));
+}
 
-    res.send(status);
-}); 
+module.exports = { readVideos, writeVideos };
+
+// Error handling
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // Initialize the server to run
 app.listen(PORT, () => {
--- routes/vidoes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-// const { get } = require("../controllers/notesController");
-
-const router = express.Router();
-
-// router.get("/", get);
-
-module.exports = router;
